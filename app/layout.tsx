0ed9382b85e39da700fcea06656972d67a4d871a
Pancham <{panchamb63@gmail.com}+{Pancham555}@users.noreply.github.com>
2025-04-06 19:45:27 +0530

    Added an adsense meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,6 +78,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
+      <meta name="google-adsense-account" content="ca-pub-2026631357019919"/>
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-800 dark:text-white">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
